a0120c599ad3ef3377976537ed24ba5e31bc1b89
Zlatko Yankov <zlatkoyankov@gmail.com>
2016-09-17 15:52:22 +0300

    add initial setup and test setup

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zlatko on 17.9.2016 г..
+ */
+import expect from 'expect';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+  expect(true).toEqual(true);
+});
+});
